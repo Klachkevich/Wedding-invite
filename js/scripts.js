6f16d111586ee430b36fdd7c785d2ab429b883e5
Klachkevich <dima31297@gmail.com>
2024-03-26 09:16:48 +0300

    Change header

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,32 @@
 $(document).ready(function () {
 
+
+    var countDownDate = new Date("Sep 13, 2024 15:0:0").getTime();
+
+    var x = setInterval(function() {
+
+        // Get today's date and time
+        var now = new Date().getTime();
+      
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+      
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      
+        // Display the result in the element with id="demo"
+        document.getElementById("timerToWedding").innerHTML = days + "д " + hours + "ч "
+        + minutes + "м " + seconds + "с ";
+      
+        // If the count down is finished, write some text
+        if (distance < 0) {
+          clearInterval(x);
+          document.getElementById("timerToWedding").innerHTML = "EXPIRED";
+        }
+      }, 1000);
     /***************** Waypoints ******************/
 
     $('.wp1').waypoint(function () {
@@ -488,6 +515,5 @@ var MD5 = function (string) {
     }
 
     var temp = WordToHex(a) + WordToHex(b) + WordToHex(c) + WordToHex(d);
-
     return temp.toLowerCase();
 };
