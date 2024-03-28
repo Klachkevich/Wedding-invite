5141663de7d175596f6c57f1b7bcd8e7ca541166
Klachkevich <dima31297@gmail.com>
2024-03-28 15:16:45 +0300

    Updated style for site

--- js/scripts.js
+++ js/scripts.js
@@ -18,8 +18,27 @@ $(document).ready(function () {
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
       
         // Display the result in the element with id="demo"
-        document.getElementById("timerToWedding").innerHTML = days + "д " + hours + "ч "
-        + minutes + "м " + seconds + "с ";
+        document.getElementById("timerToWedding").innerHTML = 
+        "<div class='timer-item'>" + 
+            "<div class='timer-value'>" + days +
+            "</div>" +
+            "<div class='timer-name'>дней</div>" +
+        "</div>" +
+        "<div class='timer-item'>" + 
+            "<div class='timer-value'>" + hours +
+            "</div>" +
+            "<div class='timer-name'>час</div>" +
+        "</div>" + 
+        "<div class='timer-item'>" + 
+            "<div class='timer-value'>" + minutes +
+            "</div>" +
+            "<div class='timer-name'>минут</div>" +
+        "</div>" + 
+        "<div class='timer-item'>" + 
+            "<div class='timer-value'>" + seconds +
+            "</div>" +
+            "<div class='timer-name'>секунд</div>" +
+        "</div>";
       
         // If the count down is finished, write some text
         if (distance < 0) {
